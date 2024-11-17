fabab43be3bad4960e14b93341aa880d680c6c6d
Thomas du Boÿs <thomas@duboys.info>
2024-11-17 15:34:25 +0100

    fix: Update github workflow and fix import - does not break stable if develop fails - runs only if artifact not already done - create dedicated release for each new stable version - revert patch on Dockerfile - fixed on upstream - update import for appimage.ts to be conform with the last moduleResolution policy

--- patch.sh
+++ patch.sh
@@ -2,16 +2,7 @@
 
 
 set -eux
-sed -i 's,import AutoLaunch from "auto-launch";,import AutoLaunch from "auto-launch";import * as appimage from "./appimage";,g' src/electron-main.ts
+sed -i 's,import AutoLaunch from "auto-launch";,import AutoLaunch from "auto-launch";\nimport * as appimage from "./appimage.js";,g' src/electron-main.ts
 sed -i "s,global.launcher.enable();,appimage.enableAutoStart();,g" src/electron-main.ts
 sed -i "s,global.launcher.disable(),appimage.disableAutoStart(),g" src/electron-main.ts
 sed -i "s,await oldLauncher.isEnabled(),appimage.isAutoStartEnabled(),g" src/electron-main.ts
-
-# fix docker build on element-desktop
-if ! grep -q WORKDIR dockerbuild/Dockerfile
-then
-  echo 'WORKDIR /project' >> dockerbuild/Dockerfile
-fi
-
-# use Node 20 LTS on docker image
-sed -i 's,NODE_VERSION 18.19.*,NODE_VERSION 20.15.1,g' dockerbuild/Dockerfile
