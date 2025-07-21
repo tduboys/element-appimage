3c233cdd916966259bf1af82c2c0b1f4d15febc5
Thomas du Boÿs <thomas@duboys.info>
2025-07-21 15:01:50 +0200

    fix: use new appimagetool url

--- scripts/appimage/02-create_appimage.sh
+++ scripts/appimage/02-create_appimage.sh
@@ -71,7 +71,7 @@ sudo chown `whoami`:`whoami` $RT/_dist/*.AppImage
 cd $RT/_dist/.
 
 ./*.AppImage --appimage-extract
-wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod +x ./appimagetool-x86_64.AppImage
 sudo rm -rf Element*.AppImage
 
